1c7b97d944f9012eac581120cb930fd4aa78af20
liuyi-victor <liuvictor14@gmail.com>
2018-02-26 12:04:12 -0500

    Initial m2 commit

---
+++ m2/src/ecs/ECSNode.java
@@ -0,0 +1,16 @@
+package ecs;
+
+import org.apache.zookeeper.*;
+
+public class ECSNode implements IECSNode
+{
+	public String getNodeName()
+	{
+		
+	}
+	public String getNodeHost()
+	{
+		
+	}
+	
+}
